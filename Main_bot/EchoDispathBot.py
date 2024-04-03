2bd75183a26399722270000d127ce88882a1ccc5
timur438 <strokahtimur@gmail.com>
2024-04-03 23:14:34 +0300

    modified:   Main_bot/EchoDispathBot.py

--- Main_bot/EchoDispathBot.py
+++ Main_bot/EchoDispathBot.py
@@ -5,23 +5,36 @@ from aiogram.filters.command import Command
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token="6883695582:AAGc1nNOVj2okUWOhGbLVr6XixQxjs6MfWw")
+bot = Bot(token="5305020634:AAHkuIW6YT1F64idr4zWKfLetJ8-5b-GT14")
 dp = Dispatcher()
+
+promo_codes = {
+    "HELLO" : 10
+    }
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     builder = ReplyKeyboardBuilder()
     
-    builder.add(types.InlineKeyboardButton(text='Купить рассылку', callback_data="buy_echo"))
-    builder.add(types.InlineKeyboardButton(text='Мой профиль', callback_data="my_account"))
-    builder.add(types.InlineKeyboardButton(text='Ввести промокод', callback_data="promocode"))
+    builder.add(types.KeyboardButton(text='Купить рассылку', callback_data="buy_echo"))
+    builder.add(types.KeyboardButton(text='Мой профиль', callback_data="my_account"))
+    builder.add(types.KeyboardButton(text='Ввести промокод', callback_data="promocode"))
     
     await message.answer("Привет! Здесь ты можешь купить рассылку", reply_markup=builder.as_markup(),)
 
-@dp.callback_query(F.data == "promocode")
-async def promocode(callback: types.CallbackQuery):
-    await callback.message.answer("Это меню промокода, скоро здесь что-то будет")
-
+@dp.message()
+async def process_enter_promo_code(message: types.Message):
+    if message.text == "Ввести промокод":
+        await message.answer("Введите промокод:")
+    elif message.text.startswith("ПРОМОКОД:"):
+        promo_code = message.text.split(":")[1].strip().upper()
+        discount = promo_codes.get(promo_code, 0)
+        if discount:
+            await message.answer(f"Промокод '{promo_code}' принят. Ваша скидка: {discount}%")
+        else:
+            await message.answer("Неверный промокод. Попробуйте еще раз.")
+    else:
+        await message.answer(message.text)
 
 
 
